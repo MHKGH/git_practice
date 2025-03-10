1b74041cd071f46edb8bb9875695fa52154078ca
MHKGH <87201946+MHKGH@users.noreply.github.com>
2025-03-10 12:08:56 +0530

    a.sh file added today

---
+++ a.sh
@@ -0,0 +1,15 @@
+#!/bin/bash   #Shebang
+
+echo "1) To check present working directory"
+pwd  
+# present working directory
+
+echo ""
+
+echo "2) To list the files(except hidden files)"
+ls  # to list the files(except hidden files)
+
+echo""
+
+echo "3) Checking 'Learningpath' folder  permission"
+ls -ld Learningpath # to check the specific file permission
